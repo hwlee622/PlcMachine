d1a724082ae863a968c15107f914761317ddfe0f
hwlee <hwlee@yjlink.com>
2025-06-30 16:57:12 +0900

    PlcMachineModbus 추가

---
+++ PlcMachine/PlcMachine/ModbusInterface/Modbus.cs
@@ -0,0 +1,160 @@
+﻿using NModbus;
+using System;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ModbusInterface
+{
+    public class Modbus
+    {
+        private ModbusLogWriter m_logWriter;
+
+        private string m_ipAddress;
+        private const int PORT = 502;
+
+        private TcpClient m_tcpClient;
+        private UdpClient m_udpClient;
+        private ModbusFactory m_factory;
+        private IModbusMaster m_master;
+
+        public int WriteTimeout = Timeout.Infinite;
+        public int ReadTimeout = Timeout.Infinite;
+
+        public Modbus(string ip)
+        {
+            m_logWriter = new ModbusLogWriter(ip, PORT);
+
+            m_ipAddress = ip;
+        }
+
+        public void Start()
+        {
+            m_tcpClient = new TcpClient(m_ipAddress, PORT);
+            m_tcpClient.ReceiveTimeout = ReadTimeout;
+            m_factory = new ModbusFactory();
+            m_master = m_factory.CreateMaster(m_tcpClient);
+        }
+
+        public void Stop()
+        {
+            m_tcpClient?.Close();
+            m_udpClient?.Close();
+        }
+
+        public bool ReadCoil(ushort address, out bool data)
+        {
+            data = false;
+            try
+            {
+                data = m_master.ReadCoils(1, address, 1)[0];
+                return true;
+            }
+            catch (Exception ex)
+            {
+                m_logWriter.LogError(ex);
+                return false;
+            }
+        }
+
+        public bool ReadCoil(ushort address, ushort len, out bool[] data)
+        {
+            data = new bool[len];
+            try
+            {
+                data = m_master.ReadCoils(1, address, len);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                m_logWriter.LogError(ex);
+                return false;
+            }
+        }
+
+        public bool WriteCoil(ushort address, bool data)
+        {
+            try
+            {
+                m_master.WriteSingleCoil(1, address, data);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                m_logWriter.LogError(ex);
+                return false;
+            }
+        }
+
+        public bool WriteCoil(ushort address, bool[] data)
+        {
+            try
+            {
+                m_master.WriteMultipleCoils(1, address, data);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                m_logWriter.LogError(ex);
+                return false;
+            }
+        }
+
+        public bool ReadHoldingRegister(ushort address, out ushort data)
+        {
+            data = 0;
+            try
+            {
+                data = m_master.ReadHoldingRegisters(1, address, 1)[0];
+                return true;
+            }
+            catch (Exception ex)
+            {
+                m_logWriter.LogError(ex);
+                return false;
+            }
+        }
+
+        public bool ReadHoldingRegister(ushort address, ushort len, out ushort[] data)
+        {
+            data = new ushort[len];
+            try
+            {
+                data = m_master.ReadHoldingRegisters(1, address, len);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                m_logWriter.LogError(ex);
+                return false;
+            }
+        }
+
+        public bool WriteHoldingRegister(ushort address, ushort data)
+        {
+            try
+            {
+                m_master.WriteSingleRegister(1, address, data);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                m_logWriter.LogError(ex);
+                return false;
+            }
+        }
+
+        public bool WriteHoldingRegister(ushort address, ushort[] data)
+        {
+            try
+            {
+                m_master.WriteMultipleRegisters(1, address, data);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                m_logWriter.LogError(ex);
+                return false;
+            }
+        }
+    }
+}
---
+++ PlcMachine/PlcMachine/PlcMachineModbus.cs
@@ -0,0 +1,205 @@
+﻿using ModbusInterface;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PlcUtil.PlcMachine
+{
+    /// <summary>
+    /// Modbus 대상으로 한 PlcMachine
+    /// </summary>
+    public class PlcMachineModbus : PlcMachine
+    {
+        private const string HOLDING_REGISTER = "H";
+        private const string COIL = "C";
+
+        private const int MAX_HOLDING_REGISTER_ADDRESS = 10000;
+        private const int MAX_COIL_ADDRESS = 10000;
+        private const ushort WORD_SCAN_SIZE = 125;
+        private const ushort BIT_SCAN_SIZE = 2000;
+
+        private Modbus m_modbus;
+
+        private CancellationTokenSource m_cts = new CancellationTokenSource();
+
+        public PlcMachineModbus(string ipAddress, int timeout = 5000) : this()
+        {
+            m_modbus = new Modbus(ipAddress);
+            m_modbus.WriteTimeout = m_modbus.ReadTimeout = timeout;
+        }
+
+        private PlcMachineModbus()
+        {
+            _wordDataDict[HOLDING_REGISTER] = new WordData(MAX_HOLDING_REGISTER_ADDRESS);
+            _bitDataDict[COIL] = new BitData(MAX_COIL_ADDRESS);
+        }
+
+        public override void CreateDevice()
+        {
+            CloseDevice();
+
+            m_modbus.Start();
+            m_cts = new CancellationTokenSource();
+            Task.Run(() => ScanDevice(m_cts.Token));
+        }
+
+        protected override bool ScanBitData()
+        {
+            bool result = true;
+            foreach (var key in _bitDataDict.Keys)
+            {
+                var addressList = m_scanAddressData.GetScanAddress(key);
+                foreach (var address in addressList)
+                {
+                    if (!m_modbus.ReadCoil((ushort)address, BIT_SCAN_SIZE, out var data))
+                        result = false;
+
+                    _bitDataDict[key].SetData(address, data);
+                }
+            }
+            return result;
+        }
+
+        protected override bool ScanWordData()
+        {
+            bool result = true;
+            foreach (var key in _wordDataDict.Keys)
+            {
+                var addressList = m_scanAddressData.GetScanAddress(key);
+                for (int i = 0; i < addressList.Count; i++)
+                {
+                    if (!m_modbus.ReadHoldingRegister((ushort)addressList[i], WORD_SCAN_SIZE, out var data))
+                        result = false;
+
+                    _wordDataDict[key].SetData(addressList[i], data);
+                }
+            }
+            return result;
+        }
+
+        public override void CloseDevice()
+        {
+            m_cts.Cancel();
+            m_modbus.Stop();
+
+            foreach (var wordData in _wordDataDict.Values)
+                wordData.ClearData();
+            foreach (var bitData in _bitDataDict.Values)
+                bitData.ClearData();
+        }
+
+        public override void GetBitData(string address, out bool value)
+        {
+            value = false;
+            if (!ushort.TryParse(address, out var uAddress))
+                return;
+
+            if (!_bitDataDict.TryGetValue(COIL, out var bitData))
+                return;
+            m_scanAddressData.SetScanAddress(COIL, uAddress, 1, BIT_SCAN_SIZE);
+
+            value = bitData.GetData(uAddress, 1)[0];
+        }
+
+        public override void SetBitData(string address, bool value)
+        {
+            if (!ushort.TryParse(address, out var uAddress))
+                return;
+
+            if (!_bitDataDict.TryGetValue(COIL, out var bitData))
+                return;
+            m_scanAddressData.SetScanAddress(COIL, uAddress, 1, BIT_SCAN_SIZE);
+
+            if (m_modbus.WriteCoil(uAddress, value))
+                bitData.SetData(uAddress, new bool[] { value });
+        }
+
+        public override void GetWordData(int address, int length, out string value)
+        {
+            value = string.Empty;
+            if (!_wordDataDict.TryGetValue(HOLDING_REGISTER, out var plcData))
+                return;
+            m_scanAddressData.SetScanAddress(HOLDING_REGISTER, address, length, WORD_SCAN_SIZE);
+
+            ushort[] data = plcData.GetData(address, length);
+            var sb = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                var bitData = BitConverter.GetBytes(data[i]);
+                if (!BitConverter.IsLittleEndian)
+                    Array.Reverse(bitData);
+
+                sb.Append(Encoding.ASCII.GetString(bitData));
+            }
+            value = sb.ToString();
+            value = value.Trim('\0');
+        }
+
+        public override void GetWordData(int address, out short value)
+        {
+            value = 0;
+            if (!_wordDataDict.TryGetValue(HOLDING_REGISTER, out var plcData))
+                return;
+            m_scanAddressData.SetScanAddress(HOLDING_REGISTER, address, 1, WORD_SCAN_SIZE);
+
+            ushort data = plcData.GetData(address, 1)[0];
+            value = (short)data;
+        }
+
+        public override void GetWordData(int address, out int value)
+        {
+            value = 0;
+            if (!_wordDataDict.TryGetValue(HOLDING_REGISTER, out var plcData))
+                return;
+            m_scanAddressData.SetScanAddress(HOLDING_REGISTER, address, 2, WORD_SCAN_SIZE);
+
+            ushort[] data = plcData.GetData(address, 2);
+            value = (data[1] << 16) | data[0];
+        }
+
+        public override void SetWordData(int address, int length, string value)
+        {
+            if (!_wordDataDict.TryGetValue(HOLDING_REGISTER, out var plcData))
+                return;
+            m_scanAddressData.SetScanAddress(HOLDING_REGISTER, address, length, WORD_SCAN_SIZE);
+
+            if (value.Length % 2 != 0)
+                value += '\0';
+
+            while (value.Length < length * 2)
+                value += "\0\0";
+
+            ushort[] data = new ushort[length];
+            for (int i = 0; i < length; i++)
+                data[i] = (ushort)(value[1 + i * 2] << 8 | value[i * 2]);
+
+            if (m_modbus.WriteHoldingRegister((ushort)address, data))
+                plcData.SetData(address, data);
+        }
+
+        public override void SetWordData(int address, short value)
+        {
+            if (!_wordDataDict.TryGetValue(HOLDING_REGISTER, out var plcData))
+                return;
+            m_scanAddressData.SetScanAddress(HOLDING_REGISTER, address, 1, WORD_SCAN_SIZE);
+
+            ushort[] data = new ushort[] { (ushort)value };
+            if (m_modbus.WriteHoldingRegister((ushort)address, data[0]))
+                plcData.SetData(address, data);
+        }
+
+        public override void SetWordData(int address, int value)
+        {
+            if (!_wordDataDict.TryGetValue(HOLDING_REGISTER, out var plcData))
+                return;
+            m_scanAddressData.SetScanAddress(HOLDING_REGISTER, address, 2, WORD_SCAN_SIZE);
+
+            ushort[] data = new ushort[2];
+            data[0] = (ushort)(value & 0xFFFF);
+            data[1] = (ushort)((value >> 16) & 0xFFFF);
+            if (m_modbus.WriteHoldingRegister((ushort)address, data))
+                plcData.SetData(address, data);
+        }
+    }
+}
